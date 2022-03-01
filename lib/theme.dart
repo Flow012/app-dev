3ae7e1a1030ee1b6f4bcfb8de10762eb73e519dd
Flow012 <jimflow01@gmail.com>
2022-03-01 21:37:08 +0800

    index on main: a52132a pagod na si harrel

---
+++ lib/theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'color.dart';
+
+
+@immutable
+class AppTheme {
+  static const colors = AppColors();
+
+  const AppTheme._();
+
+  static ThemeData define() {
+    return ThemeData(
+      primaryColor: const Color(0xffd52a67),
+      focusColor: const Color(0xffd52ABD)
+    );
+  }
+}
