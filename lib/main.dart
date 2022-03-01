30678e43509b8dd9f58c375f5766edf48d66c7f1
HarrelFuntilon <funtilonh023@gmail.com>
2022-03-01 14:56:36 +0800

    testing process

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,51 @@
+import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Home(),
-  ));
+  runApp(const MyApp());
 }
 //hello
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("hoy",style: TextStyle(
-          fontSize: 40,
-            fontFamily: 'BeVietnamPro-L'
-        ),),
-
+    return MaterialApp(
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Hello World'),
+            centerTitle: true,
+            backgroundColor: Colors.lightBlue,
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0)),
+                  color: Colors.red,
+                ),
+                child: const Text('Hello World'),
+                padding: const EdgeInsets.all(100),
+                margin: const EdgeInsets.all(10),
+              ),
+              Container(
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10.0),
+                      topRight: Radius.circular(10.0)),
+                  color: Colors.red,
+                ),
+                child: const Text('Hello World'),
+                padding: const EdgeInsets.all(20),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Image(image: AssetImage('img/1BE.png')),
     );
   }
 }
